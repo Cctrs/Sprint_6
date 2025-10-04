9d83d00e048160327fa2f5b9da5c92d1059664a8
Cctrs <agrosmaker@gmail.com>
2025-10-04 15:51:09 +0300

    Change methods in base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,5 @@
-import allure
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-from locators.base_page_locators import *
 from conftest import *
 
 
@@ -11,17 +9,33 @@ class BasePage:
         self.driver = driver
 
     
-    @allure.step('Клик по логотипу Яндекса, переключение на новую вкладку, ожидание загрузки хедера дзена')
-    def click_yandex_logo(self):
-        self.driver.find_element(*yandex_logo).click()
+    def click_element(self, locator):
+        self.driver.find_element(*locator).click()
+
+
+    def wait_page_for_load(self, locator):
+        WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located(locator))
+
+    
+    def switch_to_new_window(self):
         WebDriverWait(self.driver, 10).until(expected_conditions.number_of_windows_to_be(2))
         all_windows = self.driver.window_handles
         new_window = all_windows[1]
         self.driver.switch_to.window(new_window)
-        WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located(dzen_header))
 
 
-    @allure.step('Клик по логотипу Самоката, переход на главную, ожидание загрузки страницы')
-    def click_scooter_logo(self):
-        self.driver.find_element(*scooter_logo).click()
-        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located(scooter_picture_area))
+    def wait_element_to_be_clickable(self, locator):
+        WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable(locator))
+
+
+    def send_keys(self, locator, data):
+        self.driver.find_element(*locator).send_keys(data)
+
+
+    def scroll_to_element(self, locator):
+        faq_header = self.driver.find_element(*locator)
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", faq_header)
+
+
+    def get_current_url(self):
+        return self.driver.current_url
